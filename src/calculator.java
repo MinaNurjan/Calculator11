a19dc333452484a4cc4f0626cc5b427217b4d6c2
Mina <mukaram0709@gmail.com>
2019-01-19 10:31:27 -0800

    ADD Mul

--- src/calculator.java
+++ src/calculator.java
@@ -1,7 +1,13 @@
 public class calculator {
-    public static double sum(double num1, double num2){
-        return num1+num2;
+    public static double sum(double num1, double num2) {
+        return num1 + num2;
     }
-    public static double substract(double num1, double num2){
-        return num1-num2;
+
+    public static double substract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double Mul(double num1, double num2) {
+        return num1 * num2;
+    }
 }
