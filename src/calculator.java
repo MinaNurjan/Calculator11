72827aece2716018f75ee83e871074d432722162
MinaNurjan <46761106+MinaNurjan@users.noreply.github.com>
2019-01-19 11:04:43 -0800

    changed

--- src/calculator.java
+++ src/calculator.java
@@ -10,4 +10,6 @@ public class calculator {
     public static double Mul(double num1, double num2) {
         return num1 * num2;
     }
+    public static double Mul(double num1, double num2) {
+        return num1 * num2;
 }
