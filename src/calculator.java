2ba0312f76d8f86ed7cd0f91722faf1cbccd1b2e
MinaNurjan <46761106+MinaNurjan@users.noreply.github.com>
2019-01-19 13:57:23 -0800

    div added

--- src/calculator.java
+++ src/calculator.java
@@ -9,5 +9,7 @@ public class calculator {
 
     public static double Mul(double num1, double num2) {
         return num1 * num2;
-
+    }
+    public static double div(double num1, double num2) {
+        return num1 / num2;
 }}
