e7289e724802a9739dc3a9214d38adb4a0c56e7f
Mina <mukaram0709@gmail.com>
2019-01-19 11:07:39 -0800

    Removed mul

--- src/calculator.java
+++ src/calculator.java
@@ -9,7 +9,5 @@ public class calculator {
 
     public static double Mul(double num1, double num2) {
         return num1 * num2;
-    }
-    public static double Mul(double num1, double num2) {
-        return num1 * num2;
-}
+
+}}
